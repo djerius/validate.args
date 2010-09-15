5b96a44c392b560d9fceade252f475b74f281326
djerius@cfa.harvard.edu <djerius@cfa.harvard.edu>
2010-09-15 14:52:02 -0400

    add test for required but not specified named parameter

--- tests/named.lua
+++ tests/named.lua
@@ -25,6 +25,25 @@ function test_optional__not_specified ()
    assert_equal( nil, foo.x )
 end
 
+function test_required__specified ()
+
+   local template = { x = { }}
+   local ok, foo = validate( template, { x = 3 } )
+
+   assert_true( ok )
+   assert_equal( 3, foo.x )
+end
+
+
+function test_required__not_specified ()
+
+   local template = { x = { }}
+   local ok, foo = validate( template, {} )
+
+   assert_false( ok )
+   assert_match( 'required but not specified', foo )
+end
+
 function test_default__but_specified ()
 
 
